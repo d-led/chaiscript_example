91b3017e337e7b386dd0a625d8b598b6e1ee08f6
Dmitry Ledentsov <dmlled@yahoo.com>
2015-01-09 22:50:52 +0100

    tracing the objects

--- src/test.cpp
+++ src/test.cpp
@@ -33,25 +33,31 @@ public:
     int my_id() const {
         return id;
     }
+public:
+    virtual ~countable() {}
 };
 template <typename T> int countable<T>::count(0);
 
-class test : public countable<test> {
+class test : public countable < test > {
+private:
+    void print(const char* fun, const char* adornment = "") {
+        std::cout << fun << adornment << "[" << this->my_id() << "]" << std::endl;
+    }
 public:
-    // test(test&&) {
-    //     std::cout << __FUNCTION__ << "(test&&)" << std::endl;        
-    // }
+    test(test&&) {
+        print(__FUNCTION__, "(test&&)");
+    }
     test(const test&) {
-        std::cout << __FUNCTION__ << "(const test&)" << std::endl;        
+        print(__FUNCTION__, "(const test&)");
     }
     test() {
-        std::cout << __FUNCTION__ << "()" << std::endl;
+        print(__FUNCTION__, "()");
     }
     ~test() {
-        std::cout << __FUNCTION__ << std::endl;
+        print(__FUNCTION__);
     }
     void foo() {
-        std::cout << "foo!" << std::endl;
+        print("foo!");
     }
 };
 
@@ -60,16 +66,18 @@ int main() {
         chaiscript::ChaiScript chai;
         chai
             .add(chaiscript::fun(&helloWorld), "helloWorld")
+            .add(chaiscript::base_class<countable<test>, test>())
             .add(chaiscript::user_type<test>(), "test")
-            .add(chaiscript::constructor<test()>(),"test")
+            .add(chaiscript::constructor<test()>(), "test")
             .add(chaiscript::constructor<test(const test&)>(), "test")
             .add(chaiscript::fun(&test::foo), "foo")
-        ;
+            .add(chaiscript::fun(&countable<test>::my_id), "my_id")
+            ;
 
         chai.eval("print(helloWorld(\"Bob\"));");
         chai.eval("var t = test(); t.foo();");
         chai.eval("var t2 = t; t2.foo();");
-        chai.eval("def test::bar() { print(\"bar!\"); } t.bar();");
+        chai.eval("def test::bar() { print(\"bar![\"+this.my_id.to_string+\"]\"); } t.bar();");
     }
     catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
